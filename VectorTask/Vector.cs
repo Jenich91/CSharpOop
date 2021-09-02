e230b16e15cf28aed78c813684d895ebd821f684
Вяткин Евгений <Jenich123@yandex.ru>
2021-09-03 03:05:44 +0700

    Начал задачу Вектор

--- VectorTask/Vector.cs
+++ VectorTask/Vector.cs
@@ -5,62 +5,158 @@ namespace VectorTask
     public class Vector
     {
         public int N { get; set; }
-        public Point pointA { get; set; }
-        public Point pointB { get; set; }
-
-        public Direction direction;
-        public enum Direction
-        {
-            ToA = 0,
-            ToB = 1
-        }
+        public double[] componets { get; set; }
 
         public Vector(int n)
         {
             if (n <= 0)
             {
-                throw new ArgumentException("Размерность вектора должна быть больше 0", nameof(n));
+                throw new ArgumentException("Ошибка: размерность вектора должна быть больше 0");
             }
 
             this.N = n;
 
+            componets = new double[n];
 
-            this.pointA = new Point(0, 0);
-            this.pointB = new Point(0, 0);
-            direction = Direction.ToA;
+            for (int i = 0; i < n; i++)
+            {
+                this.componets[i] = 0;
+            }
         }
 
-        public Vector(Vector vectorToCopy)
+        public Vector(Vector vectorSource)
         {
-            this.N = vectorToCopy.N;
-            this.pointA = vectorToCopy.pointA;
-            this.pointB = vectorToCopy.pointB;
-            this.direction = vectorToCopy.direction;
+            this.N = vectorSource.GetSize();
+
+            if (this.componets == null)
+            {
+                this.componets = new double[N];
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                this.componets[i] = vectorSource.componets[i];
+            }
         }
 
         public Vector(double[] vectors)
         {
-            this.pointA = new Point(vectors[0], vectors[1]);
-            this.pointB = new Point(vectors[2], vectors[3]);
-            direction = (Direction)(int)vectors[4];
+            componets = new double[vectors.Length];
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                this.componets[i] = vectors[i];
+            }
         }
 
         public Vector(int n, double[] vectors)
         {
-            if ((vectors.Length / 5) < n)
-            {
-                this.N = n;
-                this.pointA = new Point(0, 0);
-                this.pointB = new Point(0, 0);
-                direction = Direction.ToA;
+            componets = new double[n];
 
-                return;
+            int arraySize = vectors.Length;
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                this.componets[i] = vectors[i];
             }
 
-            this.pointA = new Point(vectors[0], vectors[1]);
-            this.pointB = new Point(vectors[2], vectors[3]);
-            direction = (Direction)(int)vectors[4];
+            if (arraySize < n)
+            {
+                for (int i = arraySize; i < n; i++)
+                {
+                    this.componets[i] = 0;
+                }
+            }
         }
+
+        public Vector()
+        {
+            this.N = 0;
+            componets = new double[0];
+        }
+
+        public int GetSize()
+        {
+            return this.componets.Length;
+        }
+
+        public override string ToString()
+        {
+            return "{" + string.Join(",", this.componets) + "}";
+        }
+
+        public void Addition(Vector vectorB)
+        {
+            int nMin = GetMinimalArrayLength(this, vectorB);
+
+            for (int i = 0; i < nMin; i++)
+            {
+                this.componets[i] = this.componets[i] + vectorB.componets[i];
+            }
+        }
+
+        public int GetMinimalArrayLength(Vector vectorA, Vector vectorB)
+        {
+            int nVectorA = vectorA.GetSize();
+            int nVectorB = vectorB.GetSize();
+            return nVectorA < nVectorB ? nVectorB : nVectorB;
+        }
+
+        public void Subtraction(Vector vectorB)
+        {
+            int nMin = GetMinimalArrayLength(this, vectorB);
+
+            for (int i = 0; i < nMin; i++)
+            {
+                this.componets[i] = this.componets[i] - vectorB.componets[i];
+            }
+        }
+
+        public void Multiply(double scalarValue)
+        {
+            int dimensionsNumber = this.GetSize();
+
+            for (int i = 0; i < dimensionsNumber; i++)
+            {
+                this.componets[i] = this.componets[i] * scalarValue;
+            }
+        }
+
+        public void Reverse()
+        {
+            int dimensionsNumber = this.GetSize();
+
+            for (int i = 0; i < dimensionsNumber; i++)
+            {
+                this.componets[i] *= -1;
+            }
+        }
+
+        public double Length()
+        {
+            double result = 0;
+            int dimensionsNumber = this.GetSize();
+
+            for (int i = 0; i < dimensionsNumber; i++)
+            {
+                result += Math.Pow(this.componets[i], 2);
+            }
+
+            return Math.Sqrt(result);
+        }
+
+        public double this[int i]
+        {
+            get
+            {
+                return this.componets[i];
+            }
+            set
+            {
+                this.componets[i] = value;
+            }
+        }
+
 
     }
 }
--- VectorTask/VectorTask.cs
+++ VectorTask/VectorTask.cs
@@ -6,10 +6,131 @@ namespace VectorTask
     {
         static void Main(string[] args)
         {
-            int n = Int32.Parse(Console.ReadLine());
-            double[] vectors = { 1, 2, 3, 4};
-            Vector vector = new Vector(n, vectors);
-            Console.WriteLine(vector.pointA.X + "," + vector.pointA.Y + "-" + vector.pointB.X + "," + vector.pointB.Y);
+            Console.WriteLine("Введите размерность вектора");
+            //int n = Int32.Parse(Console.ReadLine());
+
+            try
+            {
+                // Тесты конструкторов
+                Vector vector1 = new Vector(2);
+
+                string testResult = GetTestResultMessage((vector1.componets[0] == 0 && vector1.componets[1] == 0), 1);
+                Console.WriteLine(testResult);
+
+                Vector vector2 = new Vector(vector1);
+
+                testResult = GetTestResultMessage(vector2.Equals(vector1), 2);
+                Console.WriteLine(testResult);
+
+                double[] array = { 3, 0, -10.4 };
+                vector1 = new Vector(array);
+
+                testResult = GetTestResultMessage((vector1.componets[0] == array[0] && vector1.componets[1] == array[1] && vector1.componets[2] == array[2]), 3);
+                Console.WriteLine(testResult);
+
+                array = new double[] { 3.5, 45.53, 40.932 };
+                vector1 = new Vector(4, array);
+
+                testResult = GetTestResultMessage((vector1.componets[0] == array[0] && vector1.componets[1] == array[1] && vector1.componets[2] == array[2] && vector1.componets[3] == 0), 4);
+                Console.WriteLine(testResult);
+
+                // Метод getSize()
+
+                array = new double[] { 5, 43, 32 };
+                vector1 = new Vector(array);
+
+                testResult = GetTestResultMessage((vector1.GetSize() == array.Length), 5);
+                Console.WriteLine(testResult);
+
+                // Метод toString()
+
+                array = new double[] { 0, 99, -1000, 2.0 };
+                vector1 = new Vector(array);
+
+                testResult = GetTestResultMessage((vector1.ToString() == ("{" + string.Join(",", array) + "}")), 6);
+                Console.WriteLine(testResult);
+
+                // Тесты методов
+
+                // Прибавление к вектору другого вектора
+
+                array = new double[] { 20, 299, 0, 2.20 };
+                double[] array2 = new double[] { 3, 53.35, -5 };
+
+                vector1 = new Vector(array);
+                vector2 = new Vector(array2);
+
+                vector1.Addition(vector2);
+
+                int nMin = GetMinimalArrayLength(array, array2);
+
+                for (int i = 0; i < nMin; i++)
+                {
+                    array[i] += array2[i];
+                }
+
+                testResult = GetTestResultMessage((vector1.ToString() == ("{" + string.Join(",", array) + "}")), 7);
+                Console.WriteLine(testResult);
+
+                // Вычитание из вектора другого вектора
+
+                vector1.Subtraction(vector2);
+
+                nMin = GetMinimalArrayLength(array, array2);
+
+                for (int i = 0; i < nMin; i++)
+                {
+                    array[i] -= array2[i];
+                }
+
+                testResult = GetTestResultMessage((vector1.ToString() == ("{" + string.Join(",", array) + "}")), 8);
+                Console.WriteLine(testResult);
+
+                // Умножение вектора на скаляр
+
+                int scalarValue = 2;
+                vector1.Multiply(scalarValue);
+
+                int size = array.Length;
+
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = array[i] * scalarValue;
+                }
+
+                testResult = GetTestResultMessage((vector1.ToString() == ("{" + string.Join(",", array) + "}")), 9);
+                Console.WriteLine(testResult);
+
+                // Разворот вектора
+
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Write("Метод: ");
+                Console.WriteLine(ex.TargetSite);
+                Console.Write("Вывод стека: ");
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        public static string GetTestResultMessage(bool testCase, int testId)
+        {
+            if (testCase)
+            {
+                return $"Test№" + testId + " - Ok";
+            }
+            else
+            {
+                return $"Test№" + testId + " - False";
+            }
+        }
+
+        public static int GetMinimalArrayLength(double[] array1, double[] array2)
+        {
+            return array1.Length < array2.Length ? array1.Length : array2.Length;
         }
     }
 }

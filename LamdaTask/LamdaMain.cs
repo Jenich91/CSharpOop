2b65f3332e397d4cf44eee97b4d21e93f9155ac8
Вяткин Евгений <Jenich123@yandex.ru>
2021-09-26 01:40:05 +0700

    Продолжаю делать задачу лямда

---
+++ LamdaTask/LamdaMain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LamdaTask
+{
+    class LamdaMain
+    {
+        static void Main(string[] args)
+        {
+            // А) получить список уникальных имен
+            var persons = new List<Person>()
+            {
+                new Person("Иван", 34),
+                new Person("Иван", 13),
+                new Person("Дарья", 34),
+                new Person("Пётр", 34),
+                new Person("Дарья", 14),
+            };
+
+            var uniqNameList = persons.Select(person => person.name).Distinct().ToList();
+
+            // Б) вывести список уникальных имен
+            Console.WriteLine("Имена: " + string.Join(", ", uniqNameList.Select(name => name)) + ".");
+
+            // В) получить список людей младше 18, посчитать для них средний возраст
+
+            var underagePersonList = persons.Where(person => person.age < 18)
+                                            .Select(person => person.name)
+                                            .ToList();
+
+            double averageAge = persons.Select(person => person.age).Where(age => age < 18).Average();
+
+            Console.WriteLine("Cписок людей младше 18: " + string.Join(", ", underagePersonList.Select(name => name)) + ".");
+            Console.WriteLine("Средний возраст: " + averageAge);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Г) при помощи группировки получить Map, в котором ключи –
+            //имена, а значения –средний возраст
+
+            //Д) получить людей, возраст которых от 20 до 45, вывести в консоль
+            //их имена в порядке убывания возраста
+        }
+    }
+}
---
+++ LamdaTask/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LamdaTask
+{
+    public class Person : IEquatable<Person>
+    {
+        public string name { get; set; }
+
+        public int age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} - возраст: {age}";
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 17;
+            int hash = 1;
+
+            hash = prime * hash + age.GetHashCode();
+            hash = prime * hash + name.GetHashCode();
+
+            return hash;
+        }
+
+        public bool Equals(Person obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(obj, null) || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Person person = (Person)obj;
+
+            return age.Equals(person.age) && name.Equals(person.name);
+        }
+    }
+}
